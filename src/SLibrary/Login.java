eab85174b773a657a7d779475afb54293e994f7e
Pheonix <danisha222002@gmail.com>
2022-12-27 13:54:40 +0530

    Change the bg color of login form

--- src/SLibrary/Login.java
+++ src/SLibrary/Login.java
@@ -40,7 +40,7 @@ public class Login extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(204, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(51, 255, 255));
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
         jLabel1.setText("UserName");
